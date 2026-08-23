4bbcef144dd893aa6d6459866af3df03d2a829a4
ducknife <darkisknight126@gmail.com>
2026-08-23 18:26:13 +0700

    updating'

--- project/src/main/java/com/ducknife/project/modules/user/UserController.java
+++ project/src/main/java/com/ducknife/project/modules/user/UserController.java
@@ -1,93 +1,93 @@
-package com.ducknife.project.modules.user;
+// package com.ducknife.project.modules.user;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.SortDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.data.domain.Page;
+// import org.springframework.data.domain.Pageable;
+// import org.springframework.data.domain.Sort;
+// import org.springframework.data.web.PageableDefault;
+// import org.springframework.data.web.SortDefault;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.security.core.annotation.AuthenticationPrincipal;
+// import org.springframework.security.oauth2.jwt.Jwt;
+// import org.springframework.validation.annotation.Validated;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.ducknife.project.common.ApiResponse;
-import com.ducknife.project.common.validation.group.OnCreate;
-import com.ducknife.project.common.validation.group.OnUpdate;
-import com.ducknife.project.modules.order.dto.OrderResponse;
-import com.ducknife.project.modules.user.dto.UserRequest;
-import com.ducknife.project.modules.user.dto.UserResponse;
+// import com.ducknife.project.common.ApiResponse;
+// import com.ducknife.project.common.validation.group.OnCreate;
+// import com.ducknife.project.common.validation.group.OnUpdate;
+// import com.ducknife.project.modules.order.dto.OrderResponse;
+// import com.ducknife.project.modules.user.dto.UserRequest;
+// import com.ducknife.project.modules.user.dto.UserResponse;
 
-import lombok.RequiredArgsConstructor;
+// import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequestMapping("/api/users")
-@RequiredArgsConstructor
-public class UserController {
+// @RestController
+// @RequestMapping("/api/users")
+// @RequiredArgsConstructor
+// public class UserControllerA {
 
-    private final UserService userService;
+//     private final UserService userService;
 
-    @GetMapping
-    public ResponseEntity<ApiResponse<Page<UserResponse>>> getUsers(
-            @PageableDefault(page = 0, sort = "fullname", direction = Sort.Direction.DESC) Pageable pageable) {
-        return ApiResponse.ok(userService.getUsers(pageable));
-    }
+//     @GetMapping
+//     public ResponseEntity<ApiResponse<Page<UserResponse>>> getUsers(
+//             @PageableDefault(page = 0, sort = "fullname", direction = Sort.Direction.DESC) Pageable pageable) {
+//         return ApiResponse.ok(userService.getUsers(pageable));
+//     }
 
-    @GetMapping("/test")
-    public ResponseEntity<ApiResponse<List<UserResponse>>> getUsersByFullname(
-        @RequestParam String keyword
-    ) {
-        return ApiResponse.ok(userService.getUserByFullname(keyword));
-    }
+//     @GetMapping("/test")
+//     public ResponseEntity<ApiResponse<List<UserResponse>>> getUsersByFullname(
+//         @RequestParam String keyword
+//     ) {
+//         return ApiResponse.ok(userService.getUserByFullname(keyword));
+//     }
 
-    @GetMapping("/sort")
-    public ResponseEntity<ApiResponse<List<UserResponse>>> getUserByIdLessThan(
-            @RequestParam(required = false) Long id,
-            @SortDefault(sort = "id", direction = Sort.Direction.DESC) Sort sort) {
-        return ApiResponse.ok(userService.getUsersByIdLessThan(id, sort));
-    }
+//     @GetMapping("/sort")
+//     public ResponseEntity<ApiResponse<List<UserResponse>>> getUserByIdLessThan(
+//             @RequestParam(required = false) Long id,
+//             @SortDefault(sort = "id", direction = Sort.Direction.DESC) Sort sort) {
+//         return ApiResponse.ok(userService.getUsersByIdLessThan(id, sort));
+//     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<UserResponse>> getUserById(@PathVariable Long id) {
-        return ApiResponse.ok(userService.getUserById(id));
-    }
+//     @GetMapping("/{id}")
+//     public ResponseEntity<ApiResponse<UserResponse>> getUserById(@PathVariable Long id) {
+//         return ApiResponse.ok(userService.getUserById(id));
+//     }
 
-    @GetMapping("/{id}/orders")
-    public ResponseEntity<ApiResponse<List<OrderResponse>>> findOrdersById(@PathVariable Long id) {
-        return ApiResponse.ok(userService.findOrdersById(id));
-    }
+//     @GetMapping("/{id}/orders")
+//     public ResponseEntity<ApiResponse<List<OrderResponse>>> findOrdersById(@PathVariable Long id) {
+//         return ApiResponse.ok(userService.findOrdersById(id));
+//     }
 
-    @GetMapping("/me")
-    public ResponseEntity<ApiResponse<UserResponse>> getMe(@AuthenticationPrincipal Jwt jwt) {
-        return ApiResponse.ok(userService.getMe(jwt));
-    }
+//     @GetMapping("/me")
+//     public ResponseEntity<ApiResponse<UserResponse>> getMe(@AuthenticationPrincipal Jwt jwt) {
+//         return ApiResponse.ok(userService.getMe(jwt));
+//     }
 
-    @PostMapping
-    public ResponseEntity<ApiResponse<UserResponse>> addUser(@RequestBody @Validated(OnCreate.class) UserRequest user) {
-        UserResponse savedUser = userService.addUser(user);
-        return ApiResponse.created(savedUser);
-    }
+//     @PostMapping
+//     public ResponseEntity<ApiResponse<UserResponse>> addUser(@RequestBody @Validated(OnCreate.class) UserRequest user) {
+//         UserResponse savedUser = userService.addUser(user);
+//         return ApiResponse.created(savedUser);
+//     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<String>> updateUser(@PathVariable Long id, @RequestBody @Validated(OnUpdate.class) UserRequest user) {
-        userService.updateUser(id, user);
-        return ApiResponse.ok("Cập nhật người dùng thành công!");
-    }
+//     @PutMapping("/{id}")
+//     public ResponseEntity<ApiResponse<String>> updateUser(@PathVariable Long id, @RequestBody @Validated(OnUpdate.class) UserRequest user) {
+//         userService.updateUser(id, user);
+//         return ApiResponse.ok("Cập nhật người dùng thành công!");
+//     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        userService.deleteUserById(id);
-        return ResponseEntity.noContent().build();
-    }
+//     @DeleteMapping("/{id}")
+//     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+//         userService.deleteUserById(id);
+//         return ResponseEntity.noContent().build();
+//     }
 
-}
+// }
